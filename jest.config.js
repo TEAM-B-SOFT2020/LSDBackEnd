5a407fc1ecb6e0c8cac3f3368b77750e4fb000cd
Pernille <36620361+pernillelorup@users.noreply.github.com>
2020-11-20 14:41:03 +0100

    Quick Fix Commit

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  roots: ["/src/test"],
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
+  preset: "ts-jest",
+  globals: {
+    "ts-jest": {
+      diagnostics: false,
+    },
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
-  moduleFileExtensions: ["ts", "js", "json", "node"],
-  collectCoverage: true,
-  clearMocks: true,
-  coverageDirectory: "coverage",
 };
